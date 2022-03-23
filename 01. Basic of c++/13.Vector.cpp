3a032af749fcf3a36ada07ecedc72f86bb093591
aasif-iqbal <aasif.iqbal3000@gmail.com>
2022-03-23 17:04:59 +0530

    Vector code  added

--- 01. Basic of c++/13.Vector.cpp
+++ 01. Basic of c++/13.Vector.cpp
@@ -11,3 +11,41 @@ the array. Removing the last element takes only constant time because no resizin
 Inserting and erasing at the beginning or in the middle is linear in time.
 
 */ 
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void display(vector<int> &v1)
+{
+	for(int i = 0; i <v1.size(); i++)
+	{
+		cout <<v1[i] << " ";
+	}
+	cout <<endl;
+}
+
+int main() {
+
+	vector<int> vec1;
+	int element;
+	
+	// int length;
+	// cin>>length;
+	
+	// while(length--){
+	// 	cin>>element;
+	// 	vec1.push_back(element);
+
+	// }
+
+	cout<< "Enter the value in vector";
+	for(int i = 0; i < 5; i++)
+	{
+		cin >> element;
+		vec1.push_back(element);
+	}
+	display(vec1);
+	return 0;
+}
