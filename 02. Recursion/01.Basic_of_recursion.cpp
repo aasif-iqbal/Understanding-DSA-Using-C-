0825aa879a385ce51e2480a657c23a811a9024b6
aasif-iqbal <aasif.iqbal3000@gmail.com>
2022-03-29 22:52:59 +0530

    recursion TYPE  added

--- 02. Recursion/01.Basic_of_recursion.cpp
+++ 02. Recursion/01.Basic_of_recursion.cpp
@@ -7,4 +7,13 @@ Using recursive algorithm, certain problems can be solved quite easily.
 Examples of such problems are Towers of Hanoi (TOH), Inorder/Preorder/Postorder
 Tree Traversals, DFS of Graph, etc.
 
+
+Type:
+A. Direct Recursion
+1.head recursion
+2.tail recursion
+3.tree recursion
+4.nested recursion
+
+B.Indirect recursion
 */ 
